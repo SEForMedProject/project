0116beca1a8892c6bee50452206ff4af450ec168
ewrfcas <caochenjie@quyiyuan.com>
2017-03-17 15:12:43 +0800

    首页搜索（模糊查询：疾病、症状、身体部位）

--- src/main/java/com/medical/repository/DiseaseRepository.java
+++ src/main/java/com/medical/repository/DiseaseRepository.java
@@ -2,6 +2,7 @@ package com.medical.repository;
 
 import com.medical.model.jpa.JPADisease;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ import java.util.List;
  * Created by ewrfcas on 2017/3/14.
  */
 public interface DiseaseRepository extends JpaRepository<JPADisease,String> {
-    List<JPADisease> findByNameContaining(String name);
+    @Query(value="select * from tb_ill where name like %?1% or symptom like %?1% or body_part like %?1%" ,nativeQuery=true)
+    List<JPADisease> findContaining(String name);
 }
