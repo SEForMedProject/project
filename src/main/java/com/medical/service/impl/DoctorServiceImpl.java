c5e8718464f2bf87ca193dd47a467f8ab30041dd
ewrfcas <caochenjie@quyiyuan.com>
2017-03-26 12:44:45 +0800

    医生关联

--- src/main/java/com/medical/service/impl/DoctorServiceImpl.java
+++ src/main/java/com/medical/service/impl/DoctorServiceImpl.java
@@ -1,13 +1,16 @@
 package com.medical.service.impl;
 
-import com.medical.repository.DoctorRepository;
 import com.medical.model.Doctor;
 import com.medical.model.jpa.JPADoctor;
+import com.medical.repository.DoctorRepository;
 import com.medical.service.DoctorService;
 import com.medical.util.Response;
 import com.medical.util.ResponseStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ewrfcas on 2017/3/2.
@@ -18,19 +21,26 @@ public class DoctorServiceImpl implements DoctorService{
     private transient DoctorRepository doctorRepository;
 
     @Override
-    public Response<Doctor> getDoctorByName(String doctorName){
-        Response<Doctor> response=new Response<Doctor>();
+    public Response<List<Doctor>> getDoctorByDiseaseName(String diseaseName){
+        Response<List<Doctor>> response=new Response<>();
         try{
-            JPADoctor jpaDoctor= doctorRepository.getDoctorByName(doctorName);
-            if(jpaDoctor==null){
+            List<JPADoctor> jpaDoctors= doctorRepository.findByContaining(diseaseName);
+            if(jpaDoctors.size()==0){
                 response.setStatus(ResponseStatus.FAIL);
                 response.setMessage("未找到该医生");
                 return response;
             }
-            Doctor doctor=new Doctor();
-            doctor.setName(jpaDoctor.getDoctor_name());
-            doctor.setAge(jpaDoctor.getDoctor_age());
-            response.setData(doctor);
+            List<Doctor> doctors=new ArrayList<>();
+            for(JPADoctor jpaDoctor:jpaDoctors){
+                Doctor doctor=new Doctor();
+                doctor.setId(jpaDoctor.getId());
+                doctor.setName(jpaDoctor.getName());
+                doctor.setGoodAt(jpaDoctor.getGoodAt());
+                doctor.setTitle(jpaDoctor.getTitle());
+                doctor.setIntroduction(jpaDoctor.getIntroduction());
+                doctors.add(doctor);
+            }
+            response.setData(doctors);
             response.setStatus(ResponseStatus.SUCCESS);
             response.setMessage("医生数据获取成功");
             return response;
