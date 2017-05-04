01138e019fe794926b2a604709a247da897c7046
ewrfcas <caochenjie@quyiyuan.com>
2017-05-04 16:35:53 +0800

    界面美化，多字段查询，疼痛规范化待改进（暂时注释掉了）

--- src/main/java/com/medical/controller/restcontroller/DiseaseController.java
+++ src/main/java/com/medical/controller/restcontroller/DiseaseController.java
@@ -30,23 +30,53 @@ public class DiseaseController {
     public Response<List<DiseaseForSearch>> getDiseaseByName(String diseaseName) {
         Response<List<DiseaseForSearch>> response=new Response<>();
         List<DiseaseForSearch> diseaseForSearches=new ArrayList<>();
-        if (diseaseName.contains("痛")||(diseaseName.contains("疼"))){
-            List<DiseaseForSearch> diseaseForSearches1=diseaseService.getDiseaseByName(diseaseName).getData();
-            if(diseaseForSearches1!=null&&diseaseForSearches1.size()!=0)diseaseForSearches.addAll(diseaseForSearches1);
-            if(diseaseName.contains("痛")){
-                diseaseName=diseaseName.replaceAll("痛","疼");
-            }else{
-                diseaseName=diseaseName.replaceAll("疼","痛");
-            }
-            diseaseForSearches1=diseaseService.getDiseaseByName(diseaseName).getData();
-            if(diseaseForSearches1!=null&&diseaseForSearches1.size()!=0)diseaseForSearches.addAll(diseaseForSearches1);
-            Collections.sort(diseaseForSearches, indexSortForDisease);
-            response.setStatus(ResponseStatus.SUCCESS);
-            response.setMessage("success");
-            response.setData(diseaseForSearches);
-            return response;
+//        if (diseaseName.contains("痛")||(diseaseName.contains("疼"))){
+//            List<DiseaseForSearch> diseaseForSearches1=diseaseService.getDiseaseByName(diseaseName).getData();
+//            if(diseaseForSearches1!=null&&diseaseForSearches1.size()!=0)diseaseForSearches.addAll(diseaseForSearches1);
+//            if(diseaseName.contains("痛")){
+//                diseaseName=diseaseName.replaceAll("痛","疼");
+//            }else{
+//                diseaseName=diseaseName.replaceAll("疼","痛");
+//            }
+//            diseaseForSearches1=diseaseService.getDiseaseByName(diseaseName).getData();
+//            if(diseaseForSearches1!=null&&diseaseForSearches1.size()!=0)diseaseForSearches.addAll(diseaseForSearches1);
+//            Collections.sort(diseaseForSearches, indexSortForDisease);
+//            response.setStatus(ResponseStatus.SUCCESS);
+//            response.setMessage("success");
+//            response.setData(diseaseForSearches);
+//            return response;
+//        }
+        String[] diseaseNames=diseaseName.split(" ");
+        if(diseaseNames.length==1){
+            return diseaseService.getDiseaseByName(diseaseName);
         }
-        else return diseaseService.getDiseaseByName(diseaseName);
+        List<Response<List<DiseaseForSearch>>> responseList=new ArrayList<>();
+        for (String s: diseaseNames){
+            Response<List<DiseaseForSearch>> responseTemp=diseaseService.getDiseaseByName(s);
+            responseList.add(responseTemp);
+        }
+        for (DiseaseForSearch diseaseForSearch: responseList.get(0).getData()){
+            boolean flag=false;
+            for (int i=1;i<responseList.size();i++){
+                if(responseList.get(i).getData().contains(diseaseForSearch)){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag){
+                diseaseForSearches.add(diseaseForSearch);
+            }
+        }
+        if(diseaseForSearches.size()==0){
+            for(int i=0;i<responseList.size();i++){
+                diseaseForSearches.addAll(responseList.get(i).getData());
+            }
+        }
+        Collections.sort(diseaseForSearches, indexSortForDisease);
+        response.setStatus(ResponseStatus.SUCCESS);
+        response.setMessage("success");
+        response.setData(diseaseForSearches);
+        return response;
     }
 
     @RequestMapping(value = "/disease/getDiseaseDetailById", method = RequestMethod.GET)
